815f59811f69cc16a6cda30c17349cba7ff885c8
ingridgomez <43049022+ingridgomez@users.noreply.github.com>
2019-03-10 20:24:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+$(document).ready(function() {
+  setTimeout(function(){
+    $(".powerbutton").addClass("alarm"); 
+    $(".secondsbutton").addClass("flashing"); 
+    $(".body").addClass("alarming"); 
+    $(".screen").addClass("alert"); 
+    $(".volbar").addClass("hidden");
+    $(".voltext2").addClass("hidden");
+    $(".chbar").addClass("hidden");
+    $(".chtext2").addClass("hidden");
+    $(".chnumber").addClass("hidden");
+    $("audio").get(0).play();
+  }, 10000);
 
-$(document).ready(function(){
-	console.log("ready");
-	$(".pupsillhouette").hover(function(){
-		$(".puplabel").toggleClass("show");
-	});
+    $(".powerbutton").click(function() { 
+    $(".powerbutton").removeClass("alarm");
+    $(".secondsbutton").removeClass("flashing"); 
+    $(".body").removeClass("alarming"); 
+    $(".screen").removeClass("alert"); 
+    $(".volbar").removeClass("hidden");
+    $(".voltext2").removeClass("hidden");
+    $(".chbar").removeClass("hidden");
+    $(".chtext2").removeClass("hidden");
+    $(".chnumber").removeClass("hidden");
+    $("audio").get(0).pause();
+	$("audio").get(0).currentTime = 0;
+  });
 
-	$(".doggosillhouette").hover(function(){
-		$(".doggolabel").toggleClass("show");
-	});
+  setInterval(function(){
+    var time = new Date();
+    var minutes = time.getMinutes() % 12+1;
+    $(".chnumber").text(minutes);
+    if (!$(".screen").hasClass("alert")){
+    	$(".screen").removeClass().addClass("screen").addClass("g"+minutes);
+    }
+  },1000)
 
-	$(".woofersillhouette").hover(function(){
-		$(".wooferlabel").toggleClass("show");
-	});
 });
