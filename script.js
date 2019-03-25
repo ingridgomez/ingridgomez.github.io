a10bcf5c1b17f233c0ff661e8d179252ea055c5c
ingridgomez <43049022+ingridgomez@users.noreply.github.com>
2019-03-24 21:17:46 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
-$(document).ready(function() {
-  setTimeout(function(){
-    $(".powerbutton").addClass("alarm"); 
-    $(".secondsbutton").addClass("flashing"); 
-    $(".body").addClass("alarming"); 
-    $(".screen").addClass("alert"); 
-    $(".windowbox").addClass("help"); 
-    $(".volbar").addClass("hidden");
-    $(".voltext2").addClass("hidden");
-    $(".chbar").addClass("hidden");
-    $(".chtext2").addClass("hidden");
-    $(".chnumber").addClass("hidden");
-    $("audio").get(0).play();
-  }, 1000);
-
-    $(".powerbutton").click(function() { 
-    $(".powerbutton").removeClass("alarm");
-    $(".secondsbutton").removeClass("flashing"); 
-    $(".body").removeClass("alarming"); 
-    $(".screen").removeClass("alert"); 
-    $(".windowbox").removeClass("help"); 
-    $(".volbar").removeClass("hidden");
-    $(".voltext2").removeClass("hidden");
-    $(".chbar").removeClass("hidden");
-    $(".chtext2").removeClass("hidden");
-    $(".chnumber").removeClass("hidden");
-    $("audio").get(0).pause();
-	$("audio").get(0).currentTime = 0;
-  });
-
-  setInterval(function(){
-    var time = new Date();
-    var minutes = time.getMinutes() % 12+1;
-    $(".chnumber").text(minutes);
-    if (!$(".screen").hasClass("alert")){
-    	$(".screen").removeClass().addClass("screen").addClass("g"+minutes);
-    }
-  },1000);
-
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({attackRadius: 100, monsterHitRadius: 100, attackDuration:600, jumpHeight: 300});
 });
