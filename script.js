941d6bc94ff832281224b43cb5e727f47d3e2a3f
ingridgomez <43049022+ingridgomez@users.noreply.github.com>
2019-03-28 14:48:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({attackRadius: 100, monsterHitRadius: 100, attackDuration:600, jumpHeight: 300});
+  mugen.initialize({attackRadius: 20, monsterHitRadius: 100, attackDuration:300, jumpHeight: 250, monsterSpeed: 550});
 });
