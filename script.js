c8d3b2fbc6608c1c3e01b19d0846cf315321e20b
ingridgomez <43049022+ingridgomez@users.noreply.github.com>
2019-03-28 18:07:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+var myCommands= function() {
+$(".background").addClass("special");
+};
+
+var restoreBackground= function(){
+$(".background").removeClass("special");
+};
+
+
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({attackRadius: 20, monsterHitRadius: 100, attackDuration:300, jumpHeight: 250, monsterSpeed: 550});
+  mugen.initialize({ 
+  	titleScreenImageURL: "splashscreen.png",
+  	attackRadius: 20,
+  	 monsterHitRadius: 100, attackDuration:300,
+  	  jumpHeight: 250, 
+  	  monsterSpeed: 650, 
+  	  onMonsterSpawn: myCommands, 
+  	  onMonsterWasHit: restoreBackground
+  	});
 });
