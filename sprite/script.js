2b04d61fa43638074db9684c3ae1d51fa4e73950
ingridgomez <43049022+ingridgomez@users.noreply.github.com>
2019-04-01 18:06:59 -0400

    Add files via upload

--- sprite/script.js
+++ sprite/script.js
@@ -10,7 +10,7 @@ $(".background").removeClass("special");
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
   mugen.initialize({ 
-  	titleScreenImageURL: "giphy.gif",
+  	titleScreenImageURL: "splashscreen.png",
   	attackRadius: 20,
   	 monsterHitRadius: 100, attackDuration:400,
   	  jumpHeight: 250, 
